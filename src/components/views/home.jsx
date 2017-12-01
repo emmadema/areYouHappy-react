81f185fa2397ec038b99c5ab94f822a5b9155f65
emmadema <emma.demango@gmail.com>
2017-11-30 17:40:11 -0700

    it works

--- src/components/views/home.jsx
+++ src/components/views/home.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import { browserHistory } from 'react-router';
 
+
 export default class Home extends Component {
+
   componentDidMount() {
     browserHistory.push('/');
   }
+
   render() {
+
     return (
       <div id="home">
+          <button type="button" onClick={() => browserHistory.push(`contact`)}>Sad</button>
+          <button type="button" onClick={() => browserHistory.push(`happy`)}>Happy</button>
         This is the home page.
       </div>
     );
