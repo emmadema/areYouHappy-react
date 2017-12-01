3a6c2f21177feec588cc08ceb5805afb7ded781e
emmadema <emma.demango@gmail.com>
2017-11-30 17:15:12 -0700

    first commit

---
+++ src/components/views/happy.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Happy extends Component {
+  render() {
+    return (
+      <div id="happy">
+        You are happy.
+      </div>
+    );
+  }
+}
---
+++ src/routes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/app';
+import Home from './components/views/home';
+import Contact from './components/views/contact';
+import Happy from './components/views/happy';
+
+export default (
+  <Route path='/' component={App}>
+    <IndexRoute component={Home} />
+    <Route path='contact' component={Contact} />
+    <Route path='happy' component={Happy} />
+    <Route path='*' component={Home} />
+  </Route>
+);
