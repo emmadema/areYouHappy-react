37f4d8637c22c52a6d4d62c07b46bd39cdf4ae64
emmadema <emma.demango@gmail.com>
2017-12-03 15:39:26 -0700

    testing image layout

--- src/components/views/contact.jsx
+++ src/components/views/contact.jsx
@@ -9,37 +9,121 @@ export default class Contact extends Component {
       			<div className="article" key="key1">
       				<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
       				<div className="middle">
-      					<div className="text">Testing</div>
+      					<div className="text"><a href="https://www.youtube.com/watch?v=aXItOY0sLRY">Breath Deep</a></div>
       				</div>
       			</div>
 			    <div className="article" key="key2">
 			    	<img src="https://i.imgur.com/YOtytAUb.jpg" height="200px" width="220px" className="image" />
 			    	<div className="middle">
-      					<div className="text">Testing</div>
+      					<div className="text"><a href="https://themighty.com/2017/10/funny-memes-anxiety-depression/">Laugh A Litte</a></div>
       				</div>
 			    </div>
 			    <div className="article" key="key3">
-			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<img src="http://www.metrocrisisservices.org/library/images/SPL_logo_220x240_ENG.png" height="200px" width="220px" className="image" />
 			    	<div className="middle">
-      					<div className="text">Testing</div>
+      					<div className="text"><a href="https://suicidepreventionlifeline.org/">Need to talk?</a></div>
       				</div>
 			    </div>
 			    <div className="article" key="key4">
 			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
 			    	<div className="middle">
-      					<div className="text">Testing</div>
+      					<div className="text"><a href="">Look at these cuties</a></div>
       				</div>
 			    </div>
 			    <div className="article" key="key5">
 			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
 			   		<div className="middle">
-      					<div className="text">Testing</div>
+      					<div className="text"><a href="">Go</a></div>
       				</div>
 			    </div>
 			    <div className="article" key="key6">
 			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
 			    	<div className="middle">
-      					<div className="text">Testing</div>
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key7">
+      				<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+      				<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+      			</div>
+			    <div className="article" key="key8">
+			    	<img src="https://i.imgur.com/YOtytAUb.jpg" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key9">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key10">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key11">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			   		<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key12">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>	
+			    <div className="article" key="key13">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key14">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			   		<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key15">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>	
+			    <div className="article" key="key16">
+			    		<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			   		<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key17">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>	
+			    <div className="article" key="key18">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key19">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			   		<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key20">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
       				</div>
 			    </div>	       
 		    </StackGrid>
--- src/components/views/happy.jsx
+++ src/components/views/happy.jsx
@@ -1,17 +1,134 @@
 import React, { Component } from "react";
-import StackGrid from "react-stack-grid";
+import StackGrid from 'react-stack-grid';
+
 
 export default class Happy extends Component {
+
   render() {
     return (
-      <StackGrid id="happy" columnWidth={160} gutterWidth={80} gutterHeight={20}>
-      			<div key="key1"><img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px"/></div>
-			    <div key="key2"><img src="https://i.imgur.com/YOtytAUb.jpg" height="200px" width="220px" /></div>
-			    <div key="key3"><img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px"/></div>
-			    <div key="key4"><img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" /></div>
-			    <div key="key5"><img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" /></div>
-			    <div key="key6"><img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" /></div>	       
+    	<StackGrid id="contact" className="container" columnWidth={160} gutterWidth={80} gutterHeight={20}>
+      			<div className="article" key="key1">
+      				<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+      				<div className="middle">
+      					<div className="text"><a href="https://www.youtube.com/watch?v=aXItOY0sLRY">Breath Deep</a></div>
+      				</div>
+      			</div>
+			    <div className="article" key="key2">
+			    	<img src="https://i.imgur.com/YOtytAUb.jpg" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="https://themighty.com/2017/10/funny-memes-anxiety-depression/">Laugh A Litte</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key3">
+			    	<img src="http://www.metrocrisisservices.org/library/images/SPL_logo_220x240_ENG.png" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="https://suicidepreventionlifeline.org/">Need to talk?</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key4">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Look at these cuties</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key5">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			   		<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key6">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key7">
+      				<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+      				<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+      			</div>
+			    <div className="article" key="key8">
+			    	<img src="https://i.imgur.com/YOtytAUb.jpg" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key9">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key10">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key11">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			   		<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key12">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>	
+			    <div className="article" key="key13">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key14">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			   		<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key15">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>	
+			    <div className="article" key="key16">
+			    		<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			   		<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key17">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>	
+			    <div className="article" key="key18">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key19">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			   		<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>
+			    <div className="article" key="key20">
+			    	<img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+			    	<div className="middle">
+      					<div className="text"><a href="">Go</a></div>
+      				</div>
+			    </div>	       
 		    </StackGrid>
+ 
     );
   }
 }
