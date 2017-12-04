5622cedcb6b985c99b9174acada07f0ab137ed28
emmadema <emma.demango@gmail.com>
2017-12-03 23:16:08 -0700

    the cubes have links

--- src/components/views/happy.jsx
+++ src/components/views/happy.jsx
@@ -3,8 +3,10 @@ import 'aframe';
 import 'aframe-animation-component';
 import 'aframe-particle-system-component';
 import 'babel-polyfill';
+import 'aframe-href-component'
 import {Entity, Scene} from 'aframe-react';
 import ReactDOM from 'react-dom';
+import {browserHistory} from 'react-router'
 // import StackGrid from 'react-stack-grid';
 // import ReactPlayer from 'react-player';
 
@@ -12,7 +14,7 @@ import ReactDOM from 'react-dom';
 export default class Happy extends Component {
   constructor(props) {
     super(props);
-    this.state = {color: 'red'};
+    this.state = {color: 'blue'};
   }
 
   changeColor() {
@@ -20,7 +22,13 @@ export default class Happy extends Component {
     this.setState({
       color: colors[Math.floor(Math.random() * colors.length)]
     });
+
+ 
   }
+     goToSite() {
+
+    	browserHistory.replace();
+    }
   render() {
     return (
     	<Scene>
@@ -33,10 +41,45 @@ export default class Happy extends Component {
         <Entity primitive="a-light" type="ambient" color="#445451"/>
         <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
         <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-        <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
-        <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
+        <Entity particle-system={{preset: 'dust', particleCount: 2000}}/>
+        <Entity text={{value: 'Take a Look Around!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
 
-       
+        <Entity id="box"
+          geometry={{primitive: 'box'}}
+          material={{color: this.state.color, opacity: 0.6}}
+          position={{x: 0, y: 1, z: -3}}
+          href= 'https://google.com'
+          events={{click: this.changeColor.bind(this)}}>
+        </Entity>
+
+        <Entity id="box"
+          geometry={{primitive: 'box'}}
+          material={{color: this.state.color, opacity: 0.6}}
+          position={{x: 5, y: 1, z: 1}}
+          href= 'https://google.com'
+          events={{click: this.changeColor.bind(this)}}>
+
+        </Entity>
+
+		<Entity id="box"
+          geometry={{primitive: 'box'}}
+          material={{color: this.state.color, opacity: 0.6}}
+          position={{x: 4, y: 5, z: -1}}
+          events={{click: this.changeColor.bind(this)}}>
+
+        </Entity>
+
+
+		<Entity id="box"
+          geometry={{primitive: 'box'}}
+          material={{color: this.state.color, opacity: 0.6}}
+          position={{x: -4, y: 3, z: -1}}
+          events={{click: this.changeColor.bind(this)}}>
+        </Entity>
+
+        <Entity primitive="a-camera">
+          <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
+        </Entity>
       </Scene>
  
     );
