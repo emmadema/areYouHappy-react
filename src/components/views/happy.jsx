5135b653a8476357d3b33b7acd38bd58f75a5569
emmadema <emma.demango@gmail.com>
2017-12-04 23:01:56 -0700

    adding articles

--- src/components/views/happy.jsx
+++ src/components/views/happy.jsx
@@ -17,21 +17,21 @@ export default class Happy extends Component {
               </div>
             </div>
           <div className="article" key="key2">
-            <img src="https://i.imgur.com/YOtytAUb.jpg" height="200px" width="220px" className="image" />
+            <img src="https://i.imgur.com/N9oOvUi.jpg" height="200px" width="220px" className="image" />
             <div className="middle">
-                <div className="text"><a href="https://themighty.com/2017/10/funny-memes-anxiety-depression/">Laugh A Litte</a></div>
+                <div className="text"><a href="https://www.huffingtonpost.com/entry/11-answers-to-how-can-i-help-someone-with-depression_us_5813c9ede4b08301d33e096f">Get Someone Help</a></div>
               </div>
           </div>
           <div className="article" key="key3">
-            <img src="http://www.metrocrisisservices.org/library/images/SPL_logo_220x240_ENG.png" height="200px" width="220px" className="image" />
+            <img src="https://i.imgur.com/Bz5k6dK.jpg" height="200px" width="220px" className="image" />
             <div className="middle">
-                <div className="text"><a href="https://suicidepreventionlifeline.org/">Need to talk?</a></div>
+                <div className="text"><a href="http://ndpr.nd.edu/news/experiences-of-depression-a-study-in-phenomenology/">Learn</a></div>
               </div>
           </div>
           <div className="article" key="key4">
-            <img src="https://i.imgur.com/Huou7Gh.gif" height="200px" width="220px" className="image" />
+            <img src="http://www.metrocrisisservices.org/library/images/SPL_logo_220x240_ENG.png" height="200px" width="220px" className="image" />
             <div className="middle">
-                <div className="text"><a href="">Look at these cuties</a></div>
+                <div className="text"><a href="https://suicidepreventionlifeline.org/">Immediate Help</a></div>
               </div>
           </div>
           <div className="article" key="key5">
